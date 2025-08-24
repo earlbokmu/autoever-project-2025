ecc2448aa07612c15165ecd45bd4c08ba8a1aff5
earlbokmu <kkha0426@gmail.com>
2025-08-24 21:46:35 +0900

    Dto 빌더 오류 수정

--- src/main/java/com/project/autoever/dto/UserInfoResponseDto.java
+++ src/main/java/com/project/autoever/dto/UserInfoResponseDto.java
@@ -1,10 +1,14 @@
 package com.project.autoever.dto;
 
 import com.project.autoever.entity.User;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class UserInfoResponseDto {
     private String account;
