847274dd92e189f9e2fe66154c059844c6d690ec
earlbokmu <kkha0426@gmail.com>
2025-08-24 14:10:51 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/com/project/autoever/AutoeverProject2025Application.java
@@ -0,0 +1,13 @@
+package com.project.autoever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoeverProject2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoeverProject2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/autoever/AutoeverProject2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.autoever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoeverProject2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
